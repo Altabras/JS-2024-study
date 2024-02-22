7d8fe386be9f7e7891b813caff17a0881b14e550
Altabras <Altabras2222@gmail.com>
2024-02-22 11:59:33 +0200

    new homework html/js file

---
+++ hw1.js
@@ -0,0 +1,50 @@
+
+// 1 //
+
+let a = "hello";
+let b = "owu";
+let c = "com";
+let d = "ua";
+let e = 1;
+let k = 10;
+let l = -999;
+let m = 123;
+let n = 3.14;
+let f = 2.7;
+let s = 16;
+let y = true;
+let o = false;
+
+console.log(a,b,c,d,e,k,l,m,n,f,s,y,o);
+
+
+// 2 //
+
+
+let firstName = "Eduard ";
+let middleName = "Eduardovich ";
+let lastName = "Hleba";
+
+let person = firstName +middleName + lastName;
+
+console.log(person);
+
+
+// 3 //
+
+let aa = 100;
+let bb = '100';
+let cc = true;
+
+console.log(typeof aa);
+console.log(typeof bb);
+console.log(typeof cc);
+
+
+// 4 //
+
+let yourName = prompt("Enter your name");
+let yourMiddleName = prompt("Enter your middle name");
+let age = prompt("Enter your age");
+
+console.log(yourName, yourMiddleName, age);
